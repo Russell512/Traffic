c7e230b39dfd017ebdda9b9659c1fe079e4d033c
Bluebook88 <164017530+Bluebook88@users.noreply.github.com>
2024-08-18 22:20:23 +0800

    rivadavia square (2024/8/18)

---
+++ rivadavia square_20240816/configuration.py
@@ -0,0 +1,37 @@
+import os
+
+NET_XML = 'rs.net.xml'
+ROU_XML = 'rs.rou.xml'
+ADD_XML = 'rs.add.xml'
+SUMOCFG = 'rs.sumocfg'
+
+SRC_PATH = 'source\\'
+HIS_PATH = 'history\\'
+if not os.path.isdir(HIS_PATH):
+    os.mkdir(HIS_PATH)
+
+import shutil
+shutil.copyfile(SRC_PATH + NET_XML, HIS_PATH + NET_XML)
+shutil.copyfile(SRC_PATH + ROU_XML, HIS_PATH + ROU_XML)
+
+NET_XML = SRC_PATH + NET_XML
+ROU_XML = SRC_PATH + ROU_XML
+ADD_XML = SRC_PATH + ADD_XML
+SUMOCFG = SRC_PATH + SUMOCFG
+
+import xml.etree.ElementTree as ET
+
+rou_xml = ET.parse(ROU_XML)
+routes = rou_xml.getroot()
+VEHICLES = len(routes.findall('vehicle'))
+
+sumocfg = ET.parse(SUMOCFG)
+configuration = sumocfg.getroot()
+SIM_TIME = int(configuration.find('time/end').get('value'))
+
+add_xml = ET.parse(ADD_XML)
+additional = add_xml.getroot()
+PHASE = 0
+for phase in additional.iter('phase'):
+    if(phase.get('state').find('y') == -1):
+        PHASE += 1
